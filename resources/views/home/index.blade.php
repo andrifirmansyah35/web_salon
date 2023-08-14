a613ca2bef54a3b9270a4ea0dd8899a1f398b0b1
andrifirmansyah35 <ahsyandri@gmail.com>
2023-08-14 19:17:47 +0700

    berhasil selesaikan error

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta name="description" content="" />
     <meta name="author" content="" />
-    <title>RCSM Bantul</title>
+    <title>RCSM Bantul Anjass Kelas</title>
     <!-- Favicon-->
     <link rel="icon" type="image/x-icon" href="{{ asset('home/assets/favicon.ico') }}" />
     <!-- Font Awesome icons (free version)-->
